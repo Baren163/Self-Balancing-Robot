73588abbd63481c8f0cc2bc02cafaa84cdf0b980
Beren Eskildsen <besk864@aucklanduni.ac.nz>
2024-09-16 13:18:21 +1200

    Created repository and skeleton for robot code

---
+++ Robot_Code.ino
@@ -0,0 +1,18 @@
+
+// Include libraries, define variables
+
+#include <avr/io.h>
+
+//#include <Wire.h> // The Wire library is what Arduino uses to communicate with I2C devices however I will be creating my own I2C driver
+
+// TWI Functions
+
+void setup() {
+  // Setup I2C registers for recieving data, initialise variables, set digital I/O pins
+
+}
+
+void loop() {
+ // Set I2C recieved data to variables, calculate angle of rotational displacement for the X axis, Implement PID control for motor speed
+
+}
