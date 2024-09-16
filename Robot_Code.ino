a0fa4af396807e4d2a2a9aaec83c6e6bf5cffb4d
Beren Eskildsen <besk864@aucklanduni.ac.nz>
2024-09-16 13:36:51 +1200

    Function to initialise TWI peripheral

--- Robot_Code.ino
+++ Robot_Code.ino
@@ -2,17 +2,43 @@
 // Include libraries, define variables
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define CPU_clock 16000000
 
 //#include <Wire.h> // The Wire library is what Arduino uses to communicate with I2C devices however I will be creating my own I2C driver
 
 // TWI Functions
 
+//  Initialise the TWI peripheral
+void twiInitialise(uint8_t bitRateGenerator) {
+
+  Serial.begin(9600);
+
+  // Activate internal pullups for twi
+  digitalWrite(SDA, 1);
+  digitalWrite(SCL, 1);
+
+  TWBR |= bitRateGenerator;  // Setting TWBR to 18 for a SCL frequency of 100kHz
+
+  TWSR &= !(1 << TWPS1) & !(1 << TWPS0);  // Setting pre scaler bits to zero (Pre scaler = 1)
+
+}
+
 void setup() {
   // Setup I2C registers for recieving data, initialise variables, set digital I/O pins
+
+  
+  pinMode(4, OUTPUT);
 
 }
 
 void loop() {
  // Set I2C recieved data to variables, calculate angle of rotational displacement for the X axis, Implement PID control for motor speed
 
+  digitalWrite(4, HIGH);
+  delay(500);
+  digitalWrite(4, LOW);
+  delay(500);
+  
 }
